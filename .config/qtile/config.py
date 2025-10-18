206fc9e022cff945e73c044204ab70da97d7b429
Dhiaa Brahim <dhiaabrahim1@outlook.com>
2025-10-18 20:45:33 +0100

    Add a gitignore

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -97,7 +97,7 @@ for vt in range(1, 8):
     )
 
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in "123456"]
 
 for i in groups:
     keys.extend(
@@ -140,7 +140,7 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans",
+    font="Ubuntu Bold",
     fontsize=12,
     padding=3,
 )
